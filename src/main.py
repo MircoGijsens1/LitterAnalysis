7cf4eeca541eadef6c7f4da8e6f4e056fd68ef2d
MircoGijsens1 <mirco.gijsens@gmail.com>
2025-05-03 03:37:11 +0200

    Upload firebase credentials

--- src/main.py
+++ src/main.py
@@ -1,26 +1,123 @@
 import flet as ft
+import os
+import json
+import shutil
+from flet import FilePickerUploadFile
+import asyncio
 
 
+app_data_path = os.getenv("FLET_APP_STORAGE_DATA")
 def main(page: ft.Page):
-    counter = ft.Text("0", size=50, data=0)
-
-    def increment_click(e):
-        counter.data += 1
-        counter.value = str(counter.data)
-        counter.update()
-
-    page.floating_action_button = ft.FloatingActionButton(
-        icon=ft.Icons.ADD, on_click=increment_click
+    sucess_message = ft.SnackBar(
+        duration="3000",
+        content=ft.Text("✅ Upload successful!", color=ft.Colors.GREEN),
+        bgcolor=ft.Colors.ON_SURFACE_VARIANT
     )
-    page.add(
-        ft.SafeArea(
-            ft.Container(
-                counter,
-                alignment=ft.alignment.center,
+    def show_success():
+        page.open(sucess_message)
+        page.update()
+        
+
+    error_message = ft.SnackBar(
+        duration="3000",
+        content=ft.Text("Something went wrong", color=ft.Colors.RED),
+        bgcolor=ft.Colors.ON_SURFACE_VARIANT
+    )
+    async def show_error():
+        page.open(error_message)
+        page.update()
+
+    #Settings components
+    #Firebase
+    def firebase_credentials_result(e: ft.FilePickerResultEvent):
+        print("Selected files:", e.files)
+        print("Selected file or directory:", e.path)
+        if e.files:
+            selected_firebase_text.value = f"Selected: {e.files[0].name}"
+        else:
+            selected_firebase_text.value = "No file selected."
+        page.update()
+
+    def firebase_credentials_upload(e):
+        try:
+            upload_list = []
+            if page.platform == "web":
+                if firebase_credentials_picker.result != None and firebase_credentials_picker.result.files != None:
+                    
+                    upload_list.append(
+                        FilePickerUploadFile(
+                            firebase_credentials_picker.result.files[0].name,
+                            upload_url=page.get_upload_url("FirebaseCredentials/credentials.json", 600),
+                        )
+                    )
+                    firebase_credentials_picker.upload(upload_list)
+            else:
+                if firebase_credentials_picker.result and firebase_credentials_picker.result.files:
+                    selected_file = firebase_credentials_picker.result.files[0]
+                    src = selected_file.path  # local file path
+                    dst = os.path.join(app_data_path, "FirebaseCredentials", "credentials.json")
+                    os.makedirs(os.path.dirname(dst), exist_ok=True)
+                    shutil.copy(src, dst)
+            show_success()
+            page.close(firebase_settings)
+        except Exception as e:
+            show_error()
+
+    firebase_credentials_picker = ft.FilePicker(on_result=firebase_credentials_result)
+    selected_firebase_text = ft.Text("No file selected.")
+    page.overlay.append(firebase_credentials_picker)
+
+    firebase_settings = ft.AlertDialog(
+        modal=True,
+        title=ft.Text("Please upload the firebase credentials"),
+        content=ft.Container(
+            content=ft.Column([
+                    ft.ElevatedButton("Choose file...",
+                              on_click=lambda _: firebase_credentials_picker.pick_files(allow_multiple=False, allowed_extensions=["json"])),
+                    selected_firebase_text
+                ], tight=True),
+            width=300,
+            height=120,
+            alignment=ft.alignment.center
+        ),
+        actions=[
+            ft.TextButton("Upload", on_click=firebase_credentials_upload),
+            ft.TextButton("Cancel", on_click=lambda e: page.close(firebase_settings)),
+        ],
+        actions_alignment=ft.MainAxisAlignment.END,
+        on_dismiss=lambda e: print("Modal dialog dismissed!"),
+    )
+    analysis_model_settings = ft.AlertDialog(
+        modal=True,
+        title=ft.Text("Please select an analysis model, the default value will be used"),
+        content=ft.Text("Do you"),
+        actions=[
+            ft.TextButton("Save", on_click=lambda e: page.close(analysis_model_settings)),
+            ft.TextButton("Cancel", on_click=lambda e: page.close(analysis_model_settings)),
+        ],
+        actions_alignment=ft.MainAxisAlignment.END,
+        on_dismiss=lambda e: print("Modal dialog dismissed!"),
+    )
+        
+    page.title = "Litter analysis"
+    page.appbar = ft.AppBar(
+        title=ft.Text("Litter analysis"),
+        bgcolor=ft.Colors.SURFACE_CONTAINER_HIGHEST,
+        actions=[
+            ft.PopupMenuButton(
+                content= ft.Container(
+                            content=ft.Text("Settings"),
+                            padding=ft.padding.only(right=10)
+                        ),
+                items=[
+                    ft.PopupMenuItem(text="Firebase credentials", on_click=lambda e: page.open(firebase_settings)),
+                    ft.PopupMenuItem(text="Litter",on_click=lambda e: page.open(analysis_model_settings)),
+                ]
             ),
-            expand=True,
-        )
+        ],
     )
+    page.add(ft.Text("Body!"))
 
 
-ft.app(main)
+print(app_data_path)
+ft.app(main, upload_dir=app_data_path)
