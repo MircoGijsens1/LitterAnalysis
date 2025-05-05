56bab36f91bae2e51b488f9078dd89fba6effbf2
MircoGijsens1 <mirco.gijsens@gmail.com>
2025-05-05 04:13:23 +0200

    Get Images

--- src/main.py
+++ src/main.py
@@ -7,26 +7,38 @@ from datetime import datetime, timedelta, timezone
 import asyncio
 from firebase_admin import credentials, initialize_app, delete_app, get_app, _apps
 from firebase_admin import firestore, storage, firestore_async
+from google.cloud.storage import Client, transfer_manager
+from google.oauth2 import service_account
 import pandas as pd
+from collections import defaultdict
 
 app_data_path = os.getenv("FLET_APP_STORAGE_DATA")
 settings_path = os.path.join(app_data_path, "settings.json")
 
 db = None
-bucket = None
+#bucket = None
+gcs_client = None
+gcs_bucket = None
+#print(storage.transfer_manage)
 
 def reinitialize_firebase(credential_path, bucket_name):
-    global db, bucket
+    global db, gcs_client, gcs_bucket
     try: 
         if _apps:
             delete_app(get_app())
         
-        cred = credentials.Certificate(os.path.join(app_data_path, credential_path))
+        full_cred_path = os.path.join(app_data_path, credential_path)
+        cred = credentials.Certificate(full_cred_path)
         initialize_app(cred, {
             "storageBucket": bucket_name
         })
         db = firestore_async.client()
-        bucket = storage.bucket()
+        #bucket = storage.bucket()
+        gcs_credentials = service_account.Credentials.from_service_account_file(full_cred_path)
+        gcs_client = Client(credentials=gcs_credentials)
+        gcs_bucket = gcs_client.bucket(bucket_name)
+
+        print("Firebase and GCS re-initialized successfully.")
         print("Firebase re-initialized successfully.")
     except Exception as e:
         print("Firebase:", e)
@@ -226,14 +238,22 @@ def main(page: ft.Page):
                         end_date = datetime.strptime(endDate, "%d-%m-%Y").replace(tzinfo=timezone.utc) + timedelta(days=1)
                         async for document in db.collection(col.id).where("end_timestamp", ">=", start_date).where("end_timestamp", "<", end_date).stream():
                             doc_dict = document.to_dict()
-                            doc_dict["Date"] = doc_dict["end_timestamp"].date()
-                            data.append(doc_dict)
-                            data_count += 1
-                            firebase_results_message.value = f"Fetched {data_count} objects"
+                            timestamp = doc_dict.get("end_timestamp")
+                            if isinstance(timestamp, datetime):
+                                doc_dict["Date"] = timestamp.date()
+                                data.append(doc_dict)
+                                data_count += 1
+                                firebase_results_message.value = f"Fetched {data_count} objects"
+                                page.update()
+                            else:
+                                print(f"⚠️ Missing or invalid timestamp in document: {document.id}")
                             page.update()
-            if data.count == 0:
+            if len(data) == 0:
+                firebase_results_progess_bar.value = 1.0
                 firebase_results_message.value = f"No Objects present"
+                page.update()
                 return
+            print(data)
             firebase_results_message.value = f"Preparing for export to excel"
             page.update()
             results_df = pd.DataFrame(data)  
@@ -257,18 +277,27 @@ def main(page: ft.Page):
         except Exception as e:
             print(e)
             show_error(e)
+            firebase_results_progess_bar.value = 0.0
+            firebase_results_message.value = "Something went wrong fetching the results"
 
     async def get_data_from_firestore_and_storage(startDate, endDate, includeResults, includeAnnotatedImages, includeOriginalImages, exportFolder):
         progress_container.visible = True
         firebase_results_progress.visible = False
         firebase_images_progress.visible = False
         firebase_annotated_progress.visible = False
+        firebase_export_done.visible = False  
         page.update()  
         export_created_folder = os.path.join(exportFolder, f"export_{startDate}_{endDate}")
         os.makedirs(export_created_folder, exist_ok=True)
         tasks = []
         if includeResults:
             tasks.append(get_firestore_results_to_excel(startDate, endDate, export_created_folder))
+
+        if includeAnnotatedImages:
+            tasks.append(asyncio.to_thread(get_images_from_storage,startDate, endDate, "AnnotatedImages", export_created_folder))
+
+        if includeOriginalImages:
+            tasks.append(asyncio.to_thread(get_images_from_storage,startDate, endDate, "Images", export_created_folder))
 
         await asyncio.gather(*tasks)
         firebase_export_done.visible = True  
@@ -322,6 +351,84 @@ def main(page: ft.Page):
         firebase_export_done.value = f"The data is deleted from firebase"
         page.update() 
 
+    #Firebase Images
+    def get_images_from_storage(startDate, endDate, type, exportFolder):
+        try:
+            if type == "AnnotatedImages":
+                firebase_annotated_progress.visible = True
+                firebase_annotated_images_progess_bar.value = None
+                firebase_annotated_images_message.value = "Getting annotated images..."
+            else:
+                firebase_images_progress.visible = True
+                firebase_original_images_progess_bar.value = None
+                firebase_original_images_message.value = "Getting annotated images..."
+            page.update()
+            startDate = datetime.strptime(startDate, "%d-%m-%Y")
+            print(startDate)
+            endDate = datetime.strptime(endDate, "%d-%m-%Y")
+            print(endDate)
+            date_list = [
+                (startDate + timedelta(days=i)).strftime("%d-%m-%Y")
+                    for i in range((endDate - startDate).days + 1)
+            ]
+            print(date_list)
+            grouped_list = defaultdict(list)
+            for date_str in date_list:   
+                for blob in gcs_bucket.list_blobs(match_glob=f"**/{type}/{date_str}/*.*"):
+                    folder= blob.name.rsplit("/", 3)[0]
+                    grouped_list[folder].append(blob.name.removeprefix(f'{folder}/'))
+            print(grouped_list)
+            for folder, blob_list in grouped_list.items():
+                download_many_blobs_with_transfer_manager(type, "", "",folder, blob_list, exportFolder)
+            if type == "AnnotatedImages":
+                firebase_annotated_progress.visible = True
+                firebase_annotated_images_progess_bar.value = 1.0
+                firebase_annotated_images_message.value = "Annotated images complete"
+            else:
+                firebase_images_progress.visible = True
+                firebase_original_images_progess_bar.value = 1.0
+                firebase_original_images_message.value = "Images complete"
+            page.update()
+        except Exception as e:
+            print("GCS access error:", e)
+            if type == "AnnotatedImages":
+                firebase_annotated_progress.visible = True
+                firebase_annotated_images_progess_bar.value = 0.0
+                firebase_annotated_images_message.value = "Something went wrong with the annotated images"
+            else:
+                firebase_images_progress.visible = True
+                firebase_original_images_progess_bar.value = 0.0
+                firebase_original_images_message.value = "Something went wrong with the images"
+            page.update()
+    
+    def download_many_blobs_with_transfer_manager(type_images, progressBar, ProgressText, folder, blob_names, destination_directory="", workers=8):
+        """Download blobs in a list by name, concurrently in a process pool.
+
+        The filename of each blob once downloaded is derived from the blob name and
+        the `destination_directory `parameter. For complete control of the filename
+        of each blob, use transfer_manager.download_many() instead.
+
+        Directories will be created automatically as needed to accommodate blob
+        names that include slashes.
+        """
+        # The maximum number of processes to use for the operation. The performance
+        # impact of this value depends on the use case, but smaller files usually
+        # benefit from a higher number of processes. Each additional process occupies
+        # some CPU and memory resources until finished. Threads can be used instead
+        # of processes by passing `worker_type=transfer_manager.THREAD`.
+        # workers=8
+        transfer_manager.download_many_to_path(
+            gcs_bucket, blob_names, destination_directory=destination_directory, max_workers=workers, blob_name_prefix=f"{folder}/",  worker_type=transfer_manager.THREAD
+        )
+
+        """for name, result in zip(blob_names, results):
+            # The results list is either `None` or an exception for each blob in
+            # the input list, in order.
+
+            if isinstance(result, Exception):
+                print("Failed to download {} due to exception: {}".format(name, result))
+            else:
+                print("Downloaded {} to {}.".format(name, destination_directory + name))"""
 
     def delete_from_firebase(e):
         startDate =  startdate_input.value.strip()
